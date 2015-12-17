e9eba810eb792e70e26fcfc1431ab8aced68d939
dannyarosenfeld <dannyarosenfeld@gmail.com>
2015-12-17 13:35:11 +0000

    Added ability to show text posts

---
+++ app/controllers/image_links_controller.rb
@@ -0,0 +1,49 @@
+class ImageLinksController < ApplicationController
+    def index
+        @image_links = ImageLink.all
+    end
+        
+    def show
+        @image_link = ImageLink.find(params[:id])
+    end
+
+    def new
+
+    end
+
+    def edit
+        @image_link = ImageLink.find(params[:id])
+    end
+
+    def create
+        @image_link = ImageLink.new(url: params[:image_link][:url], title: params[:image_link][:title])
+    if  @image_link.save
+        redirect_to image_links_path
+    else
+         @errors = @image_link.errors
+         render :new
+    end
+    end
+
+    def update
+        @image_link = ImageLink.find(params[:id])
+    if  @image_link.update(link: params[:image_link][:url], title: params[:image_link][:title])
+        redirect_to image_links_path
+    else
+        @errors = @image_link.errors
+        render :edit
+    end
+    end
+
+    def delete
+        @image_link = ImageLink.find(params[:id])
+        @image_link.destroy
+        redirect_to image_links_path
+    end
+        
+        
+    private
+  def image_link_params
+    params.require(:image_link).permit(:url, :title)
+  end    
+end
---
+++ app/controllers/text_posts_controller.rb
@@ -0,0 +1,72 @@
+class TextPostsController < ApplicationController
+    def index
+        @text_posts = TextPost.all
+        
+    end
+    
+    def show
+        @text_post = TextPost.find(params[:id])
+    end
+
+    def new
+
+    end
+
+    def edit
+        @text_post= TextPost.find(params[:id])
+    end
+
+    def create
+        @text_post= TextPost.new(description: params[:text_post][:description], name: params[:text_post][:name])
+    if  @text_post.save
+        redirect_to text_posts_path
+    else
+         @errors = @text_post.errors
+         render :new
+    end
+    end
+
+    def update
+        @text_post = TextPost.find(params[:id])
+    if  @text_post.update(description: params[:text_post][:description], name: params[:text_post][:name])
+        redirect_to text_posts_path
+    else
+        @errors = @text_post.errors
+        render :edit
+    end
+    end
+
+    def delete
+        @text_post = TextPost.find(params[:id])
+        @text_post.destroy
+        redirect_to text_posts_path
+    end
+        
+        
+    private
+  def text_post_params
+    params.require(:text_post).permit(:description, :name)
+  end    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+end
